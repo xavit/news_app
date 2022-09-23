9fff6036a680cc249cc7c9c9aa2e99a08e5c9f18
Javier Claros <javiclaros@gmail.com>
2022-09-23 15:05:16 -0400

    UI Listado reutilizable

--- lib/src/widgets/lista_noticias.dart
+++ lib/src/widgets/lista_noticias.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/src/models/news_models.dart';
+import 'package:news_app/src/theme/tema.dart';
 
 class ListaNoticias extends StatelessWidget {
   final List<Article> noticias;
@@ -10,8 +11,145 @@ class ListaNoticias extends StatelessWidget {
     return ListView.builder(
       itemCount: noticias.length,
       itemBuilder: (BuildContext context, int index) {
-        return Text("${noticias[index].title}");
+        return _Noticia(noticia: noticias[index], index: index);
       },
+    );
+  }
+}
+
+class _Noticia extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _Noticia({required this.noticia, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _TarjetaTopBar(
+          noticia: noticia,
+          index: index,
+        ),
+        _TarjetaTitulo(noticia: noticia),
+        _TarjetaImagen(noticia: noticia),
+        _TarjetaBody(noticia: noticia),
+        const _TarjetaBotones(),
+        const SizedBox(height: 10),
+        const Divider(),
+      ],
+    );
+  }
+}
+
+class _TarjetaBotones extends StatelessWidget {
+  const _TarjetaBotones();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        RawMaterialButton(
+          onPressed: () {},
+          fillColor: miTema.colorScheme.secondary,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: const Icon(
+            Icons.star_border,
+            color: Colors.white,
+          ),
+        ),
+        RawMaterialButton(
+          onPressed: () {},
+          fillColor: Colors.blue,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: const Icon(
+            Icons.more,
+            color: Colors.white,
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class _TarjetaBody extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaBody({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Text(noticia.description ?? ''),
+    );
+  }
+}
+
+class _TarjetaImagen extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaImagen({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+        child: Container(
+          child: noticia.urlToImage != null
+              ? FadeInImage(
+                  placeholder: const AssetImage('assets/img/giphy.gif'),
+                  image: NetworkImage("${noticia.urlToImage}"),
+                )
+              : const Image(image: AssetImage('assets/img/no-image.png')),
+        ),
+      ),
+    );
+  }
+}
+
+class _TarjetaTitulo extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaTitulo({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Text("${noticia.title}",
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w700)),
+    );
+  }
+}
+
+class _TarjetaTopBar extends StatelessWidget {
+  final Article noticia;
+  final int index;
+  const _TarjetaTopBar({Key? key, required this.noticia, required this.index})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Row(children: [
+        Text(
+          "${index + 1} ",
+          style: TextStyle(color: miTema.colorScheme.secondary),
+        ),
+        Text('${noticia.source!.name}. '),
+      ]),
     );
   }
 }
