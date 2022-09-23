87047b764a8411427c0ee7bf99adc3b85df1c74f
Javier Claros <javiclaros@gmail.com>
2022-09-23 09:42:16 -0400

    BottomNavigationBar

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -4,17 +4,38 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: const BouncingScrollPhysics(),
-        children: [
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.green,
-          ),
-        ],
-      ),
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
+    );
+  }
+
+  BottomNavigationBar _Navegacion() {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'Para ti',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.public),
+          label: 'Encabezados',
+        ),
+      ],
+    );
+  }
+
+  PageView _Paginas() {
+    return PageView(
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+      ],
     );
   }
 }
