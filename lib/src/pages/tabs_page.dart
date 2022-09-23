d7867e2bd0bdc93f3261184d17f1434a2620a4dd
Javier Claros <javiclaros@gmail.com>
2022-09-23 10:31:11 -0400

    Provider como manejador de estado

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _Paginas(),
-      bottomNavigationBar: _Navegacion(),
+    return ChangeNotifierProvider(
+      create: (_) => _NavegacionModel(),
+      child: const Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+      ),
     );
   }
+}
 
-  BottomNavigationBar _Navegacion() {
+class _Paginas extends StatelessWidget {
+  const _Paginas({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+      ],
+    );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  const _Navegacion({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
     return BottomNavigationBar(
-      currentIndex: 0,
+      currentIndex: navegacionModel.paginaActual,
+      onTap: (i) => navegacionModel.paginaActual = i,
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.person_outline),
@@ -24,18 +59,15 @@ class TabsPage extends StatelessWidget {
       ],
     );
   }
+}
 
-  PageView _Paginas() {
-    return PageView(
-      physics: const NeverScrollableScrollPhysics(),
-      children: [
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.green,
-        ),
-      ],
-    );
+class _NavegacionModel with ChangeNotifier {
+  int _paginaActual = 0;
+
+  int get paginaActual => _paginaActual;
+
+  set paginaActual(int valor) {
+    _paginaActual = valor;
+    notifyListeners();
   }
 }
